a3bcbe5fcd19d14bc341152168d49e18d8d9e97f
Sheshadrinath R <sheshadrinathr@Sheshadrinaths-MacBook-Pro.local>
2018-08-29 11:08:34 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { CreateUserComponent } from './admin/create-user/create-user.component';
+import { ListUserComponent } from './admin/list-user/list-user.component';
+import { LoginComponent } from './account/login/login.component';
+import { ResetPasswordComponent } from './account/reset-password/reset-password.component';
+import { NotFoundComponent } from './shared/not-found/not-found.component';
+import { AdminComponent } from './admin/admin.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'reset-password', component: ResetPasswordComponent },
+  { path: 'admin', component: AdminComponent, children: [
+    { path: '', redirectTo: 'list-user', pathMatch: 'full' },
+    { path: 'create-user', component: CreateUserComponent },
+    { path: 'list-user', component: ListUserComponent },
+  ]},
+  { path: '**', component: NotFoundComponent }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateUserComponent,
+    ListUserComponent,
+    LoginComponent,
+    ResetPasswordComponent,
+    NotFoundComponent,
+    AdminComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
